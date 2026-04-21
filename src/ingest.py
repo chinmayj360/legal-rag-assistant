2f74cb9d31e9e0ade696f7ea1562b6632158af19
ChinmayJ <chinmayj360@gmail.com>
2026-04-22 03:15:10 +0530

    Loading the pdf and embedding using local ollama model

--- src/ingest.py
+++ src/ingest.py
@@ -1,7 +1,7 @@
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
-from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain_community.embeddings import OllamaEmbeddingss
 
 def ingest_pdf(file_path):
     loader = PyPDFLoader(file_path)
@@ -13,7 +13,7 @@ def ingest_pdf(file_path):
     )
     chunks = splitter.split_documents(documents)
 
-    embeddings = OpenAIEmbeddings()
+    embeddings = OllamaEmbeddings(model="phi3")
 
     vectordb = Chroma.from_documents(
         chunks,
