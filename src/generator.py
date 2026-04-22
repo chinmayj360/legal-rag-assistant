dff13f058f260b525dc900a7821b10ea2accdbc7
ChinmayJ <chinmayj360@gmail.com>
2026-04-22 23:09:38 +0530

    updated from local llm to API calls using Gorq

--- src/generator.py
+++ src/generator.py
@@ -1,9 +1,20 @@
-from langchain_community.chat_models import ChatOllama
+from dotenv import load_dotenv
+load_dotenv()
 
-llm = ChatOllama(model="tinyllama")
+from langchain_groq import ChatGroq
+
+llm = ChatGroq(
+    model="llama-3.3-70b-versatile",
+    temperature=0
+)
 
 def generate_answer(query, docs):
-    context = "\n\n".join([doc.page_content for doc in docs])
+    docs = docs[:3]
+
+    context = "\n\n".join([
+        doc.page_content.strip().replace("\n", " ")
+        for doc in docs
+    ])
 
     prompt = f"""
 You are a legal document assistant.
@@ -11,14 +22,16 @@ You are a legal document assistant.
 STRICT RULES:
 - Answer ONLY using the provided context
 - Do NOT make up information
-- If answer is not found, say: "Not found in document"
-- Keep answer simple and clear
+- If answer is not found, say: Not found in document
+- Keep answer short and clear
 
 Context:
 {context}
 
 Question:
 {query}
+
+Answer:
 """
 
     response = llm.invoke(prompt)
